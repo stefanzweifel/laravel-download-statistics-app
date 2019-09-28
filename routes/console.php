ca8de7b3ad343a69741d840ba6eb0f4cca3507c1
Stefan Zweifel <hello@stefanzweifel.io>
2019-09-28 22:59:10 +0200

    Add a "rebuild-everything" Artisan Command

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,3 +13,22 @@
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+Artisan::command('rebuild-everything', function () {
+
+    // Build Timeperiod for which data should be rebuilt
+    $period = collect(CarbonInterval::months(1)->toPeriod(
+        Carbon::parse('2013-05'),
+        Carbon::parse('first day of this month')->subMonth()
+    ));
+
+    // Loop through period and call artisan command to fetch data
+    foreach($period as $date) {
+        $this->info("Fetch downloads for {$date->format('F Y')}.");
+
+        Artisan::call("app:fetch-downloads --date={$date->format('Y-m')}");
+    }
+
+    $this->info('Done');
+
+});
