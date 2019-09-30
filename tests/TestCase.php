6bede99651b8eb4c485a5e7f4ec8fec01a807405
Stefan Zweifel <hello@stefanzweifel.io>
2019-09-30 20:23:24 +0200

    Revert Change in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        TestResponse::macro('data', function ($key) {
+            return $this->original->getData()[$key];
+        });
+    }
+
 }
