d814af551098f2f5c919b013f48a62d60ee035db
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-12-16 12:18:17 +0100

    Update Factory

--- app/Models/DownloadsPerMonth.php
+++ app/Models/DownloadsPerMonth.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
 class DownloadsPerMonth extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'version',
         'minor_version',
--- database/factories/DownloadsPerMonthFactory.php
+++ database/factories/DownloadsPerMonthFactory.php
@@ -1,20 +1,30 @@
 <?php
 
+namespace Database\Factories;
+
 use App\Models\DownloadsPerMonth;
 use App\Models\Version;
-use Faker\Generator as Faker;
-use Illuminate\Support\Carbon;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
 
-$factory->define(DownloadsPerMonth::class, function (Faker $faker) {
-    $date = Carbon::parse($faker->dateTimeBetween('-2 years'));
-    $version = $faker->randomElement(['v5.0.0', 'v5.1.0', 'v5.2.0', 'v5.3.0', 'v4.2.0', 'v.4.1.0']);
+class DownloadsPerMonthFactory extends Factory
+{
+    protected $model = DownloadsPerMonth::class;
 
-    return [
-        'version' => $version,
-        'minor_version' => Version::minorVersion($version),
-        'year' => $date->format('Y'),
-        'month' => $date->format('m'),
-        'date' => $date->format('Y-m'),
-        'downloads' => $faker->numberBetween(10, 999999),
-    ];
-});
+    public function definition(): array
+    {
+        $date = Carbon::parse($this->faker->dateTimeBetween('-2 years'));
+        $version = $this->faker->randomElement(['v5.0.0', 'v5.1.0', 'v5.2.0', 'v5.3.0', 'v4.2.0', 'v4.1.0']);
+
+        return [
+            'version' => $version,
+            'minor_version' => Version::minorVersion($version),
+            'year' => $date->format('Y'),
+            'month' => $date->format('m'),
+            'date' => $date->format('Y-m'),
+            'downloads' => $this->faker->numberBetween(10, 999999),
+        ];
+    }
+}
+
